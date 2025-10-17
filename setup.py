c35f98166d3cbc7cea24a22113ae1d0a32b2996e
Gonzalo Sebastián Hormazabal Haro <ghormazabal@cmpd.cl>
2025-10-17 17:35:30 -0300

    Delete libraries already contaed on glue environment

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name="py_toolbox",
-    version="0.7.0", 
+    version="0.7.1",  # <-- Versión incrementada
     description="Una colección de herramientas reutilizables para automatizaciones en Python.",
     author="Gonzalo Hormazabal", 
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    # 'boto3' y 'botocore' eliminados de la lista
     install_requires=[
-        "boto3",
-        "botocore",
         "google-api-python-client",
         "google-auth-httplib2",
         "google-auth-oauthlib",
         "tika",
         "beautifulsoup4",
+        # pandas se instalará por separado en el Glue Job
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
